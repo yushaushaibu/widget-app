d215e556e31a2e8567621d241c7238ed4e8f560e
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-18 16:16:20 -0700

    dropdown feature added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-    const renderedOptions = options.map((option) => {
-        return (
-            <div key={option.value } onClick={() => onSelectedChange(option)} className='item'>
-                {option.label}
-            </div>
-        )
-    })
+  const [open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
-        <div className='ui form'>
-            <div className='field'>
-                <label className='label'>Select a Color</label>
-                <div className='ui selection dropdown visible active'>
-                    <i className='dropdown icon'></i>
-                    <div className='text'>{selected.label}</div>
-                    <div className='menu visible transition'>
-                        {renderedOptions}
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div
+        key={option.value}
+        onClick={() => onSelectedChange(option)}
+        className="item"
+      >
+        {option.label}
+      </div>
     );
-}
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+          onClick={() => setOpen(!open)}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
