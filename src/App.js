75c8431b1657f168bc90bab7859b27b4d00883df
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-18 10:13:29 -0700

    events handlers on Dropdown components created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -38,11 +38,12 @@ const options = [
 
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div style={{padding: '2em'}}>
             {/* <Accordion items={items} /> */}
             {/* <Search /> */}
-            <Dropdown options={options}/>
+            <Dropdown options={options} selected={selected} onSelectedChange={setSelected}/>
         </div>
     )
 };
