fc5649cc94d1ee038f7ccc651ec0e15b876d2013
yushaushaibu <yushaushaibu1@gmail.com>
2022-08-17 18:56:23 -0700

    project working

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -14,16 +15,44 @@ const Search = () => {
                     format: 'json',
                     srsearch: term
                 }
-            })
+            });
+
+            setResults(data.query.search);
         }
-        search();
+        
+        if (term) {
+            search();
+        }
+
     }, [term])
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div className='item' key={result.pageid}>
+            <div className='right floated content'>
+                <a className='ui button' href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+            </div>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <div className='ui form'>
-            <div className='field'>
-                <label>Enter Search Term</label>
-                <input className='input' value={term} onChange={(e) => setTerm(e.target.value)} />
+        <div className='parent'>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Enter Search Term</label>
+                    <input className='input' value={term} onChange={(e) => setTerm(e.target.value)} />
+                </div>
+            </div>
+
+            <div className='ui celled list'>
+                {renderedResults}
             </div>
         </div>
     )
